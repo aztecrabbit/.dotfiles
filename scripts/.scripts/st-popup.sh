b3041229f32b9a1f30fcc52e21fa1480a4ee5b26
Aztec Rabbit <ars.xda@gmail.com>
2020-08-27 19:26:12 +0700

    Fix failed to open when forced to close

--- scripts/.scripts/st-popup.sh
+++ scripts/.scripts/st-popup.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
 create_st_popup() {
-    st="st -c st-popup -e"
+    st='st -c st-popup -e'
 
-    tmux attach -t st-popup
+	$st tmux new-session -A -s st-popup > /dev/null 2>&1 &
 
-    if [[ $? -eq 1 ]]; then
-        $st tmux attach -t st-popup > /dev/null 2>&1 &
-    else
-        $st tmux new -s st-popup \; split-window -v \; select-pane -t 1 \; \
-            split-window -h \; select-pane -t 1 \; resize-pane -D 24 \; attach > /dev/null 2>&1 &
-    fi
+	# $st tmux new -s st-popup \; split-window -v \; select-pane -t 1 \; \
+    #     split-window -h \; select-pane -t 1 \; resize-pane -D 24 \; attach > /dev/null 2>&1 &
 }
 
 main() {
     node_id=$(xdo id -N st-popup)
 
-    if [[ ! $node_id ]]; then
-        create_st_popup
+    if [[ -z $node_id ]]; then
+		create_st_popup
 
         sleep 0.200
 
-        node_id=$(xdo id -N st-popup)
+	    node_id=$(xdo id -N st-popup)
     fi
 
     if [[ $node_id ]]; then
-        bspc node $node_id -g hidden --focus
+		bspc node $node_id -g hidden --focus
         xdo move -x 0 -y 0 $node_id
-    fi
+	fi
 }
 
 main
