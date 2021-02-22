ea2508feac5004467be14ad3d24e7fa914efffa3
Akbar Rifai <ars.xda@gmail.com>
2021-02-22 19:29:27 +0700

    Rewrite refresh playlist, make it readeable

--- polybar/.config/polybar/scripts/refresh-playlist.sh
+++ polybar/.config/polybar/scripts/refresh-playlist.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 notify-send -t 3000 "Updating Playlist" "Please wait ..."
-# mpc stop
-# mpc clear
-find "$(cd ~/Music; pwd -P)" -type f \( -name '*.mp3' -o -name '*.flac' -o -name '*.loss' -o -name '*.aiff' -o -name '*.aif' \) -exec ls -d {} \; | sort > ~/.local/share/mpd/playlists/playlist.m3u
-# mpc load playlist
-# mpc update
-# mpc random on
-# mpc play
-# notify-send -t 3000 "Playlist Updated"
+
+music_directory="$(cd $HOME/Music; pwd -P)"
+
+music_playlist="$(find "$music_directory" -type f \( -name '*.mp3' -o -name '*.flac' -o -name '*.loss' -o -name '*.aiff' -o -name '*.aif' \) -exec ls -d {} \; | sort)"
+music_playlist_filename="$HOME/.local/share/mpd/playlists/music.m3u"
+
+cat << EOF > "$music_playlist_filename"
+#EXTM3U
+
+$music_playlist
+EOF
