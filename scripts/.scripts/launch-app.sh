2aa3a0a93b9bf2a5bab134ae4c1a7573b145261f
Akbar Rifai <ars.xda@gmail.com>
2021-04-07 16:58:49 +0700

    scripts: launch-app: Filter argument in second section

--- scripts/.scripts/launch-app.sh
+++ scripts/.scripts/launch-app.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-    notify-send -t "3000" "Shortcut not set!"
-    exit 1
-fi
-
 cmd="$1"
 app_name="$2"
 notification_timeout="$3"
+
+if [ -z "$cmd" ]; then
+    notify-send -t "3000" "Command not set!"
+    exit 1
+fi
 
 if [ "$app_name" ]; then
     notify-send -t "${notification_timeout:-"3000"}" "Launching $app_name ..."
