8c1408db9e602524737a619af84fc5d7a7088336
Akbar Rifai <ars.xda@gmail.com>
2024-01-02 23:01:27 +0700

    mpd-refresh-music: Support scan inside symlink folder

--- scripts/.scripts/mpd-refresh-music.sh
+++ scripts/.scripts/mpd-refresh-music.sh
@@ -2,9 +2,9 @@
 
 notify-send -t 3000 "Updating Music Playlist" "Please wait ..."
 
-playlist_source_directory="$(cd $HOME/Music; pwd -P)"
+music_dir="$HOME/Music"
 
-playlist="$(find "$playlist_source_directory" -type f \( -name '*.mp3' -o -name '*.flac' -o -name '*.loss' -o -name '*.aiff' -o -name '*.aif' \) -exec ls -d {} \; | sort)"
+playlist="$(cd $music_dir; find . -type f \( -name '*.mp3' -o -name '*.flac' -o -name '*.loss' -o -name '*.aiff' -o -name '*.aif' \) -exec ls -d {} \; | awk '{ print "'$music_dir/'"$0}' | sort)"
 playlist_filepath="$HOME/.local/share/mpd/playlists/music.m3u"
 
 mkdir -p "$(dirname "$playlist_filepath")"
